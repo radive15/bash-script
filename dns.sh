50055ce2a243c23aea384ca01a50c76f05093965
rifki <rrdvan15@gmail.com>
2026-04-16 18:51:51 +0700

    upgrade file dns

--- dns.sh
+++ dns.sh
@@ -1,17 +1,39 @@
-#!/bin/bash -x
+#!/bin/bash
+
+set -euo pipefail
 
 domain="k8s.local"
-zone="Z0027769AL6VVCOQ6GCX"
 binAWS="/usr/bin/aws"
-getMetadataName="$($binAWS ec2 describe-tags --filters "Name=resource-id,Values=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)" --query 'Tags[*].Value' --output text)"
-subDomain="`echo $getMetadataName | cut -d '.' -f 1`"
+
+# IMDSv2 - ambil token dulu
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+# Ambil metadata menggunakan token IMDSv2
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/instance-id)
+privateIP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/local-ipv4)
+REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+  http://169.254.169.254/latest/meta-data/placement/region)
+
+# Ambil hostname dari tag Name EC2
+getMetadataName=$($binAWS ec2 describe-tags --region "$REGION" \
+  --filters "Name=resource-id,Values=$INSTANCE_ID" \
+  --query 'Tags[?Key==`Name`].Value | [0]' --output text)
+
+# Validasi variabel kritis
+if [ -z "$privateIP" ] || [ -z "$getMetadataName" ] || [ "$getMetadataName" = "None" ]; then
+    echo "ERROR: Gagal ambil metadata dari EC2"
+    exit 1
+fi
+
+zone="${ROUTE53_ZONE_ID:-$($binAWS ssm get-parameter --region "$REGION" --name "/dns/zone-id" --query 'Parameter.Value' --output text)}"
+subDomain="$(echo "$getMetadataName" | cut -d '.' -f 1)"
 hostname=$getMetadataName
-homeDir="/home/centos"
-privateIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
 #Set Hostname
-sed -i "s/^\(HOSTNAME\s*=\s*\).*$/\1$hostname/" /etc/sysconfig/network
-hostnamectl set-hostname --static $hostname
+hostnamectl set-hostname --static "$hostname"
 echo $hostname > /etc/hostname
 sed -i 's/ - set_hostname/\#- set_hostname/' /etc/cloud/cloud.cfg
 sed -i 's/ - update_hostname/\#- update_hostname/' /etc/cloud/cloud.cfg
@@ -22,10 +44,16 @@ $privateIP $hostname $subDomain" > /etc/hosts
 
 #Set DNS
 
-getSubDomain=$(cli53 export $zone | grep -w $privateIP | grep $subDomain | head -n1 | awk '{print $1}')
-if [ "$getSubDomain" == "$subDomain" ]; then
-  cli53 rrdelete $zone $getSubDomain A
-  cli53 rrcreate $zone "$subDomain 60 A $privateIP"
-else
-  sudo cli53 rrcreate $zone "$subDomain 60 A $privateIP"
-fi
+aws route53 change-resource-record-sets \
+  --hosted-zone-id "$zone" \
+  --change-batch "{
+    \"Changes\": [{
+      \"Action\": \"UPSERT\",
+      \"ResourceRecordSet\": {
+        \"Name\": \"$subDomain.$domain\",
+        \"Type\": \"A\",
+        \"TTL\": 60,
+        \"ResourceRecords\": [{\"Value\": \"$privateIP\"}]
+      }
+    }]
+  }"
